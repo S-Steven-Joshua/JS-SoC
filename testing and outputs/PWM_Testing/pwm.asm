fe840508aa67fe4fa8d5ce39e78c2c0b24a07f87
S Steven Joshua <136824140+S-Steven-Joshua@users.noreply.github.com>
2026-06-27 01:29:44 +0530

    Update pwm.asm

--- testing and outputs/PWM_Testing/pwm.asm
+++ testing and outputs/PWM_Testing/pwm.asm
@@ -1,13 +1,11 @@
 lui   x5,0x40000
 addi  x5,x5,8
 
-lui   x6,0x00020
-addi  x6,x6,1
-sw    x6,0(x5)
+lui   x6,0x00640
+addi  x6,x6,20
 
-# 40 NOPs
-
-addi  x6,x0,0
 sw    x6,0(x5)
 
 beq   x0,x0,0
+
+// 1MHz 20% duty cycle
