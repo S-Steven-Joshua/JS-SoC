9d9d033afd757c83d2260d3f958c6145d3af62a1
S Steven Joshua <136824140+S-Steven-Joshua@users.noreply.github.com>
2026-06-27 00:53:51 +0530

    Update pwm.asm

--- testing and outputs/PWM_Testing/pwm.asm
+++ testing and outputs/PWM_Testing/pwm.asm
@@ -1,43 +1,13 @@
 lui   x5,0x40000
-addi  x5,x5,8          # x5 = 0x40000008
+addi  x5,x5,8
 
-lui   x6,0x00040
-addi  x6,x6,2          # x6 = 0x00040002
-sw    x6,0(x5)         # PWM Start
+lui   x6,0x00020
+addi  x6,x6,1
+sw    x6,0(x5)
 
-# 30 NOPs
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
-addi x0,x0,0
+# 40 NOPs
 
 addi  x6,x0,0
-sw    x6,0(x5)         # PWM Stop
+sw    x6,0(x5)
 
 beq   x0,x0,0
