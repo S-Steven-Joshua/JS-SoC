dce39c3b422511ace93f98c8c6c9621c298e8df9
S Steven Joshua <136824140+S-Steven-Joshua@users.noreply.github.com>
2026-06-27 10:41:20 +0530

    Create tb.sv

---
+++ testing and outputs/beq_testing/tb.sv
@@ -0,0 +1,61 @@
+`timescale 1ns/1ps
+
+module cpu_tb;
+
+logic clk;
+logic rst;
+
+logic [31:0] write_data;
+logic [31:0] data_add;
+logic mem_write;
+logic [31:0] pr_data;
+logic wave;
+logic wave1;
+logic wave2;
+
+top top1(
+    .clk(clk),
+    .rst(rst),
+    .write_data(write_data),
+    .data_add(data_add),
+    .mem_write(mem_write),
+    .pr_data(pr_data),
+    .wave(wave),
+    .wave1(wave1),
+    .wave2(wave2)
+);
+
+initial begin
+    clk = 0;
+    forever #1 clk = ~clk;
+end
+
+initial begin
+    rst = 1;
+    #10;
+    rst = 0;
+end
+
+always @(posedge clk) begin
+    if(!rst)
+        $display("PC=%08h  INSTR=%08h",
+                 top1.pc,
+                 top1.instr);
+end
+
+initial begin
+    #100;
+
+    $display("\n========================================");
+    $display("BEQ TEST");
+    $display("========================================");
+    $display("x1 = %0d (0x%08h)", top1.core1.data.r1.register[1], top1.core1.data.r1.register[1]);
+    $display("x2 = %0d (0x%08h)", top1.core1.data.r1.register[2], top1.core1.data.r1.register[2]);
+    $display("x3 = %0d (0x%08h)", top1.core1.data.r1.register[3], top1.core1.data.r1.register[3]);
+    $display("x4 = %0d (0x%08h)", top1.core1.data.r1.register[4], top1.core1.data.r1.register[4]);
+    $display("========================================");
+
+    $finish;
+end
+
+endmodule
